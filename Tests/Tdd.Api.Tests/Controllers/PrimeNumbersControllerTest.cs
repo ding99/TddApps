1eec3ebc74fa859e07597fde933cd0b046f50552
Wei Ding <ding_web@yahoo.co.jp>
2021-02-09 03:24:43 -0500

    two happy paths

--- Tests/Tdd.Api.Tests/Controllers/PrimeNumbersControllerTest.cs
+++ Tests/Tdd.Api.Tests/Controllers/PrimeNumbersControllerTest.cs
@@ -1,14 +1,49 @@
 using NUnit.Framework;
+using Moq;
+using Primes.API.Controllers;
+using Primes.Lib.Services;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Tdd.Api.Tests.Controllers{
-	public class PrimeNumbersTests {
-		[SetUp]
-		public void Setup() {
+	public class PrimeNumbersControllerTests {
+		private readonly Mock<IPrimeNumbers> _mock;
+		private readonly PrimeNumbersController _controller;
+
+		public PrimeNumbersControllerTests() {
+			_mock = new Mock<IPrimeNumbers>();
+			_controller = new PrimeNumbersController(_mock.Object);
 		}
 
-		[Test]
-		public void Test1() {
-			Assert.Pass();
+		[TestCase(2)]
+		[TestCase(7)]
+		[TestCase(17)]
+		[TestCase(199)]
+		[TestCase(10061)]
+		public async Task TestIsPrimeNumber_Happy_True(int number) {
+			_mock.Setup(p => p.IsPrimeNumber(number)).Returns(Task.FromResult(true));
+
+			var result = (ObjectResult)await _controller.IsPrimeNumber(number);
+
+			Assert.AreEqual(StatusCodes.Status200OK, result.StatusCode);
+			Assert.AreEqual(true, result.Value);
 		}
+
+		[TestCase(-1)]
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(99)]
+		[TestCase(10011)]
+		public async Task TestIsPrimeNumber_Happy_False(int number) {
+			_mock.Setup(p => p.IsPrimeNumber(number)).Returns(Task.FromResult(false));
+
+			var result = (ObjectResult)await _controller.IsPrimeNumber(number);
+
+			Assert.AreEqual(StatusCodes.Status200OK, result.StatusCode);
+			Assert.AreEqual(false, result.Value);
+		}
+
+		//TODO
 	}
 }
