bd1c404dc121f32bd0a75e0a4450d066d789df69
Wei Ding <ding_web@yahoo.co.jp>
2021-02-10 12:38:56 -0500

    test first prime numbers

--- Tests/Tdd.Lib.Tests/Services/PrimeNumbersServicesTest.cs
+++ Tests/Tdd.Lib.Tests/Services/PrimeNumbersServicesTest.cs
@@ -32,5 +32,28 @@ namespace Tdd.Lib.Tests.Services {
 			var result = await _operator.IsPrimeNumber(value);
 			Assert.IsFalse(result);
 		}
+
+		[TestCase(0, new int[0])]
+		[TestCase(1, new int[] { 2 })]
+		[TestCase(2, new int[] { 2, 3 })]
+		[TestCase(3, new int[] { 2, 3, 5 })]
+		[TestCase(9, new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23 })]
+		public async Task TestFirstPrimeNumbers_Happy(int number, int[] expected) {
+			var result = await _operator.FirstPrimeNumbers(number);
+			Assert.AreEqual(expected, result);
+		}
+
+		[TestCase(-1)]
+		[TestCase(-2)]
+		[TestCase(-3)]
+		[TestCase(-10)]
+		[TestCase(-100)]
+		public async Task TestFirstPrimeNumbers_Bad(int number) {
+			int[] expected = new int[0];
+
+			var result = await _operator.FirstPrimeNumbers(number);
+
+			Assert.AreEqual(expected, result);
+		}
 	}
 }
