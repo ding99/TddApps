cb28a4627461f58b928203b6f71215d1fc85f8da
Wei Ding <ding_web@yahoo.co.jp>
2021-02-10 12:22:19 -0500

    test isprime

--- Tests/Tdd.Lib.Tests/Services/PrimeNumbersServicesTest.cs
+++ Tests/Tdd.Lib.Tests/Services/PrimeNumbersServicesTest.cs
@@ -1,14 +1,36 @@
+using Moq;
 using NUnit.Framework;
+using Primes.Lib.Services;
+using Primes.Lib.Concrete;
+using System;
+using System.Threading.Tasks;
 
 namespace Tdd.Lib.Tests.Services {
 	public class PrimeNumbersServicesTests {
-		[SetUp]
-		public void Setup() {
+		private readonly PrimeNumbers _operator;
+
+		public PrimeNumbersServicesTests() {
+			_operator = new PrimeNumbers();
 		}
 
-		[Test]
-		public void Test1() {
-			Assert.Pass();
+		[TestCase(2)]
+		[TestCase(3)]
+		[TestCase(19)]
+		[TestCase(199)]
+		[TestCase(10061)]
+		public async Task TestIsPrime_Happy(int value) {
+			var result = await _operator.IsPrimeNumber(value);
+			Assert.IsTrue(result);
+		}
+
+		[TestCase(1)]
+		[TestCase(6)]
+		[TestCase(39)]
+		[TestCase(201)]
+		[TestCase(10062)]
+		public async Task TestIsPrime_Bad(int value) {
+			var result = await _operator.IsPrimeNumber(value);
+			Assert.IsFalse(result);
 		}
 	}
 }
