0600eba164ec6fa0c1f79d18d6333222d0e5f0c4
Wei Ding <ding_web@yahoo.co.jp>
2021-04-20 02:13:41 -0400

    isprime bad pathes

--- Primes.API/Controllers/PrimeNumbersController.cs
+++ Primes.API/Controllers/PrimeNumbersController.cs
@@ -20,12 +20,13 @@ namespace Primes.API.Controllers {
 		[ProducesResponseType(200)]
 		[ProducesResponseType(500)]
 		public async Task<IActionResult> IsPrimeNumber(int number) {
-			return Ok(await Task.Run(() => true));
-			//try {
-			//	return Ok(await _service.IsPrimeNumber(number));
-			//} catch(Exception e) {
-			//	return StatusCode(500, e);
-			//}
+			//return Ok(await Task.Run(() => true));
+			try {
+				return Ok(await _service.IsPrimeNumber(number));
+			}
+			catch (Exception e) {
+				return StatusCode(500, e);
+			}
 		}
 
 		[HttpGet("{number}")]
