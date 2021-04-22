c337fe7792f1db7170762d4e8c5feaea0431eab9
Wei Ding <ding_web@yahoo.co.jp>
2021-04-21 22:18:54 -0400

    implement controller for string

--- Primes.API/Controllers/ManiputeStringsController.cs
+++ Primes.API/Controllers/ManiputeStringsController.cs
@@ -11,15 +11,23 @@ namespace Primes.API.Controllers {
 	[ApiController]
 	public class ManiputeStringsController : ControllerBase {
 
-		public ManiputeStringsController(IManipulateStrings service) {
+		private readonly IManipulateStrings _service;
 
+		public ManiputeStringsController(IManipulateStrings service) {
+			_service = service;
 		}
 
 		[HttpPost("reversion/{words}")]
 		[ProducesResponseType(200)]
 		[ProducesResponseType(500)]
 		public async Task<IActionResult> ReverseWords(string words) {
-			return await Task.Run(() => Ok());
+			try {
+				return Ok(await _service.ReverseWords(words));
+			}
+			catch(Exception e) {
+				return StatusCode(500, e);
+			}
+//			return await Task.Run(() => Ok());
 		}
 	}
 }
